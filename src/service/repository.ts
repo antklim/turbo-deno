4107c9a362800f8d20c7bff0041bdd89dca23f9d
antklim <antklim@gmail.com>
2022-10-27 07:32:39 +1100

    Implementing addProduct in shopping cart service

--- src/service/repository.ts
+++ src/service/repository.ts
@@ -5,6 +5,7 @@ export interface ShoppingCart {
   addProduct(
     args: { product: Product; qty: number; receipt?: Receipt },
   ): Promise<Receipt>;
+  createCart(receipt: Receipt): Promise<void>;
   deleteProduct(args: { product: Product; receipt: Receipt }): Promise<Receipt>;
   checkout(receipt: Receipt): Promise<Receipt>;
   total(receipt: Receipt): number;
--- src/service/shopping-cart.ts
+++ src/service/shopping-cart.ts
@@ -1,5 +1,5 @@
 import { Product } from "../entity/product.ts";
-import { Receipt } from "../entity/receipt.ts";
+import { newReceipt, Receipt } from "../entity/receipt.ts";
 import { ShoppingCart } from "./repository.ts";
 
 interface AddProductArgs {
@@ -8,6 +8,14 @@ interface AddProductArgs {
   receipt?: Receipt;
 }
 
-// deno-lint-ignore no-unused-vars
-export const addProduct = (r: ShoppingCart) => {
+export const addProduct = async (repo: ShoppingCart, args: AddProductArgs) => {
+  const { receipt: r } = args;
+
+  const receipt = r ?? newReceipt();
+
+  if (!r) {
+    await repo.createCart(receipt);
+  }
+
+  return;
 };
